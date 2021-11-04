25b8c6162842d4a62e40ddf96b8a72e962bc3a98
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-04 00:22:33 -0300

    feat: add handlers for error and messages; and add id generators

---
+++ utils/handlers.js
@@ -0,0 +1,7 @@
+export const handleError = (error) => {
+  return console.log(`Warning: error message - ${error.message}`)
+};
+
+export const handleMessage = (message) => {
+  return console.log(`Message: log message - ${message}`)
+};
