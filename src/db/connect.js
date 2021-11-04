7fab260a9a6eda817ee8e0a8868e0bc14835ed6e
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-04 00:36:13 -0300

    feat: connection verification and connection creation

---
+++ src/db/connect.js
@@ -0,0 +1,15 @@
+import fs from 'fs-extra'
+import { database, fullPath } from '../../constants/index'
+import {handleMessage, handleError} from '../../utils/index'
+
+export const connection = async () => {
+  try {
+    if(!fs.existsSync(fullPath)){
+      await fs.mkdir(fullPath)
+      await fs.writeJSON(database, [])
+    }
+    return handleMessage("skyDB: started!")
+  } catch (error) {
+    return handleError(error)
+  }
+}
