faae0a3ecd71f1c0487a6ac70a6db09805a6f476
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-04 23:01:20 -0300

    feat: add function to generate id and handlers error and logs

--- utils/genId.js
+++ utils/genId.js
@@ -1,21 +1,9 @@
-function makeid(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() *
-charactersLength));
- }
- return result;
-}
-
-
 export const genId = (length) => {
-  const result = ''
+  let result = ''
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   const charLength = chars.length
-  for(let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     result += chars.charAt(Math.floor(Math.random() * charLength))
   }
   return result
-};
+}
