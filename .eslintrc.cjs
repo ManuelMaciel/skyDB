c716cf8ddb6499e351854a0d668a92c716e4f8fb
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-08 21:28:26 -0300

    style: add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest: true
   },
   extends: [
-    'standard'
+    'standard',
+    'plugin:jest/recommended'
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
   rules: {
-  }
+  },
+  plugins: ['jest']
 }
