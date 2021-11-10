ffc496004b71a96d6ba62790f2f6394bedf15590
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-10 00:25:37 -0300

    feat: add model required to create json document and validation of type and if exist

--- src/db/create.js
+++ src/db/create.js
@@ -1,18 +1,31 @@
 import fs from 'fs-extra'
 
-import { handleError, genId } from '../../utils'
-import { database } from '../../constants'
+import { handleError, handleMessage, genId } from '../../utils'
+import { fullPath } from '../../constants'
 
-export const create = async (options = {}) => {
+export const create = async (model, options = {}) => {
+  // validations
+  if(!model){
+    return handleMessage('model is required')
+  }
+  if( typeof model !== 'string'){
+    return handleMessage(`model must be of type "string" and the type provided is a ${typeof model}`)
+  }
+  if(!fs.existsSync(`${fullPath}/${model}.json`)){
+    return handleMessage(`the model ${model} does not exist`)
+  }
+  //
   try {
-    const storage = await fs.readJSON(database)
+    const storage = await fs.readJSON(`${fullPath}/${model}.json`)
 
     const updateOptions = {
       ...options,
-      id: genId
+      id: genId(10),
+      createdAt: Date.now().toString(),
+      updatedAt: Date.now().toString(),
+      deletedAt: ''
     }
-    console.log(database)
-    await fs.writeJSON(database, [...storage, updateOptions])
+    await fs.writeJSON(`${fullPath}/${model}.json`, [...storage, updateOptions])
   } catch (error) {
     return handleError(error)
   }
