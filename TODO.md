adc13cb6031c686763de7e7357d83e47f31f934e
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-11-10 00:55:42 -0300

    feat: update todo and validations by model for get function

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@
 
 ### Incompleted
 - [ ] change the term get to findById and create functions to obtain data by where ~3d #task 2021-11-10
+- [ ] get function return promise ~3d #bug 2021-11-10
 
 ### Completed ✓
 - [x] add models when creating documents
--- src/db/get.js
+++ src/db/get.js
@@ -1,12 +1,23 @@
 import fs from 'fs-extra'
 
 import { handleError } from '../../utils'
-import { database } from '../../constants'
+import { fullPath } from '../../constants'
 
-export const get = async (id) => {
+export const get = async (model, id) => {
+   // validations
+   if(!model){
+    return handleMessage('model is required')
+  }
+  if( typeof model !== 'string'){
+    return handleMessage(`model must be of type "string" and the type provided is a ${typeof model}`)
+  }
+  if(!fs.existsSync(`${fullPath}/${model}.json`)){
+    return handleMessage(`the model ${model} does not exist`)
+  }
+  //
   try {
-    const storage = await fs.readJSON(database)
-    const record = storage.find((record) => record.id === id)
+    const storage = await fs.readJSON(`${fullPath}/${model}.json`)
+    const record = await storage.find(record => record.id === id)
     return record
   } catch (error) {
     return handleError(error)
