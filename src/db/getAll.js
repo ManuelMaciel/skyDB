3080b06998f65bb641d082f58cfb07888f6a47d6
Diego Prestes <diegoprestes.git@hotmail.com>
2021-11-15 12:06:24 -0300

    fix: sort

--- src/db/getAll.js
+++ src/db/getAll.js
@@ -19,9 +19,8 @@ export const getAll = async (model, options) => {
   }
   //
   try {
-    let record;
     const storage = await fs.readJSON(`${fullPath}/${model}.json`)
-    record = storage
+    let record = storage
     // where
     if(options.where){
       record = record.filter(record => {
@@ -34,14 +33,14 @@ export const getAll = async (model, options) => {
     if(options.order){
       if(options.order[0] === 'ASC' || options.order[0] === 'asc') {
         record = record.sort((a, b) => {
-          (a[options[1]] > b[options[1]]) ? 1 : -1
+          return (a[options.order[1]] > b[options.order[1]]) ? 1 : -1
         })
       } else if (options.order[0] === 'DESC' || options.order[0] === 'desc') {
         record = record.sort((a, b) => {
-          (a[options[1]] > b[options[1]]) ? -1 : 1
+          return (a[options.order[1]] > b[options.order[1]]) ? -1 : 1
+
         })
       }
-
     }
     return record
   } catch (error) {
